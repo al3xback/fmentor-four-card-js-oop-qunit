a46a54248da62284a14c554da965bcda86f8271c
alex <al3xback@gmail.com>
2024-01-19 17:34:39 +0700

    added first commit

---
+++ js/util.js
@@ -0,0 +1,273 @@
+class Component {
+	createElementAttribute(name, value) {
+		return {
+			name,
+			value,
+		};
+	}
+
+	createElement(tag, className, text, attributes) {
+		const newElement = document.createElement(tag);
+		if (className) {
+			newElement.className = className;
+		}
+		newElement.textContent = text || '';
+		if (attributes && attributes.length > 0) {
+			for (const attr of attributes) {
+				newElement.setAttribute(attr.name, attr.value);
+			}
+		}
+		return newElement;
+	}
+}
+
+class Card {
+	id;
+	title;
+	description;
+	imageUrl;
+
+	constructor(title, description, imageUrl) {
+		this.id = this.calculateId(title);
+		this.title = title;
+		this.description = description;
+		this.imageUrl = imageUrl;
+	}
+
+	calculateId(val) {
+		return val.toLowerCase().split(' ').join('-');
+	}
+}
+
+class Header extends Component {
+	render() {
+		const headerEl = this.createElement('header');
+
+		const headerTitleEl = this.createElement(
+			'h1',
+			'sr-only',
+			'Four card feature section'
+		);
+
+		headerEl.appendChild(headerTitleEl);
+
+		return headerEl;
+	}
+}
+
+class CardItem extends Component {
+	constructor(item) {
+		super();
+		this.item = item;
+	}
+
+	render() {
+		const cardItemEl = this.createElement(
+			'div',
+			`card__list-item card__list-item--${this.item.id}`
+		);
+
+		const cardItemTitleEl = this.createElement(
+			'h3',
+			'card__title',
+			this.item.title
+		);
+
+		const cardItemDescriptionEl = this.createElement(
+			'p',
+			'card__desc',
+			this.item.description
+		);
+
+		const cardItemImageWrapperEl = this.createElement('div', 'card__image');
+
+		const cardItemImageEl = this.createElement('img', null, null, [
+			this.createElementAttribute('src', this.item.imageUrl),
+			this.createElementAttribute('alt', ''),
+			this.createElementAttribute('width', 64),
+			this.createElementAttribute('height', 64),
+		]);
+
+		cardItemImageWrapperEl.appendChild(cardItemImageEl);
+
+		cardItemEl.appendChild(cardItemTitleEl);
+		cardItemEl.appendChild(cardItemDescriptionEl);
+		cardItemEl.appendChild(cardItemImageWrapperEl);
+
+		return cardItemEl;
+	}
+}
+
+class CardList extends Component {
+	cardBlocks = [
+		{
+			items: [
+				new Card(
+					'Supervisor',
+					'Monitors activity to identify project roadblocks',
+					'./images/icons/supervisor.svg'
+				),
+			],
+		},
+		{
+			items: [
+				new Card(
+					'Team Builder',
+					'Scans our talent network to create the optimal team for your project',
+					'./images/icons/team-builder.svg'
+				),
+				new Card(
+					'Karma',
+					'Regularly evaluates our talent to ensure quality',
+					'./images/icons/karma.svg'
+				),
+			],
+		},
+		{
+			items: [
+				new Card(
+					'Calculator',
+					'Uses data from past projects to provide better delivery estimates',
+					'./images/icons/calculator.svg'
+				),
+			],
+		},
+	];
+
+	render() {
+		const cardListEl = this.createElement('div', 'card__list');
+
+		for (const cardBlock of this.cardBlocks) {
+			const cardBlockEl = this.createElement('div', 'card__list-block');
+
+			for (const item of cardBlock.items) {
+				const cardItem = new CardItem(item);
+				const cardItemEl = cardItem.render();
+
+				cardBlockEl.appendChild(cardItemEl);
+			}
+
+			cardListEl.appendChild(cardBlockEl);
+		}
+
+		return cardListEl;
+	}
+}
+
+class Section extends Component {
+	render() {
+		const sectionEl = this.createElement('section', 'section');
+
+		/* section head */
+		const sectionHeadEl = this.createElement('div', 'section__head');
+
+		const sectionHeadTitleEl = this.createElement(
+			'h2',
+			'section__title',
+			'Reliable, efficient delivery'
+		);
+
+		const sectionHeadSubtitleEl = this.createElement(
+			'p',
+			'section__subtitle'
+		);
+
+		const sectionHeadSubtitleTextEl = this.createElement(
+			'strong',
+			null,
+			'Powered by Technology'
+		);
+
+		sectionHeadSubtitleEl.appendChild(sectionHeadSubtitleTextEl);
+
+		const sectionHeadDescriptionEl = this.createElement(
+			'p',
+			'section__desc',
+			'Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful'
+		);
+
+		sectionHeadEl.appendChild(sectionHeadTitleEl);
+		sectionHeadEl.appendChild(sectionHeadSubtitleEl);
+		sectionHeadEl.appendChild(sectionHeadDescriptionEl);
+
+		/* section body */
+		const sectionBodyEl = this.createElement('div', 'section__body');
+
+		const cardList = new CardList();
+		const cardListEl = cardList.render();
+
+		sectionBodyEl.appendChild(cardListEl);
+
+		sectionEl.appendChild(sectionHeadEl);
+		sectionEl.appendChild(sectionBodyEl);
+
+		return sectionEl;
+	}
+}
+
+class Main extends Component {
+	render() {
+		const mainEl = this.createElement('main');
+
+		const mainContainerEl = this.createElement('div', 'container');
+
+		const section = new Section();
+		const sectionEl = section.render();
+
+		mainContainerEl.appendChild(sectionEl);
+
+		mainEl.appendChild(mainContainerEl);
+
+		return mainEl;
+	}
+}
+
+class Footer extends Component {
+	render() {
+		const footerEl = this.createElement('footer');
+
+		const footerContainerEl = this.createElement('div', 'container');
+
+		const footerTextEl = this.createElement('p', null, 'Challenge by ');
+
+		const footerTextLinkCreatorEl = this.createElement(
+			'a',
+			'btn btn--link',
+			'Frontend Mentor',
+			[
+				this.createElementAttribute(
+					'href',
+					'https://www.frontendmentor.io?ref=challenge'
+				),
+				this.createElementAttribute('rel', 'noopener'),
+				this.createElementAttribute('target', '_blank'),
+			]
+		);
+
+		const footerTextLinkCoderEl = this.createElement(
+			'a',
+			'btn btn--link',
+			'al3xback',
+			[
+				this.createElementAttribute(
+					'href',
+					'https://github.com/al3xback'
+				),
+				this.createElementAttribute('rel', 'noopener'),
+				this.createElementAttribute('target', '_blank'),
+			]
+		);
+
+		footerTextEl.appendChild(footerTextLinkCreatorEl);
+		footerTextEl.append('. Coded by ');
+		footerTextEl.appendChild(footerTextLinkCoderEl);
+
+		footerContainerEl.appendChild(footerTextEl);
+
+		footerEl.appendChild(footerContainerEl);
+
+		return footerEl;
+	}
+}
+
+export { Component, Card, CardItem, CardList, Header, Section, Main, Footer };
---
+++ test/main.js
@@ -0,0 +1,127 @@
+import jsdom from 'jsdom';
+
+import {
+	Component,
+	Card,
+	CardItem,
+	CardList,
+	Header,
+	Section,
+	Main,
+	Footer,
+} from '../js/util.js';
+
+const { JSDOM } = jsdom;
+const { test } = QUnit;
+
+QUnit.module('DOM', (hooks) => {
+	hooks.beforeEach(() => {
+		const { document } = new JSDOM(
+			`<!DOCTYPE html><html><body></body></html>`
+		).window;
+		global.document = document;
+	});
+
+	test("should be able to create element via 'Component' class method", (assert) => {
+		const component = new Component();
+		const headingOneEl = component.createElement(
+			'h1',
+			'title',
+			'Lorem ipsum'
+		);
+		document.body.appendChild(headingOneEl);
+
+		const isHeadingOneElElExist = !!document.querySelector('.title');
+		assert.ok(isHeadingOneElElExist);
+	});
+
+	test("should be able to return element attribute data via 'Component' class method", (assert) => {
+		const component = new Component();
+		const widthData = component.createElementAttribute('width', 640);
+
+		const expectedWidthData = {
+			name: 'width',
+			value: 640,
+		};
+
+		assert.deepEqual(widthData, expectedWidthData);
+	});
+
+	test("should be able to return card data via 'Card' class", (assert) => {
+		const rawCardData = new Card(
+			'Supervisor',
+			'Monitors activity to identify project roadblocks',
+			'./images/icons/supervisor.svg'
+		);
+
+		const cardData = { ...rawCardData };
+
+		const expectedCardData = {
+			id: 'supervisor',
+			title: 'Supervisor',
+			description: 'Monitors activity to identify project roadblocks',
+			imageUrl: './images/icons/supervisor.svg',
+		};
+
+		assert.deepEqual(cardData, expectedCardData);
+	});
+
+	test("should be able to create card item element via 'CardItem' class", (assert) => {
+		const cardData = new Card(
+			'Supervisor',
+			'Monitors activity to identify project roadblocks',
+			'./images/icons/supervisor.svg'
+		);
+		const cardItem = new CardItem(cardData);
+		const cardItemEl = cardItem.render();
+		document.body.appendChild(cardItemEl);
+
+		const isCardItemElExist = !!document.querySelector('.card__list-item');
+		assert.ok(isCardItemElExist);
+	});
+
+	test("should be able to create card list element via 'CardList' class", (assert) => {
+		const cardList = new CardList();
+		const cardListEl = cardList.render();
+		document.body.appendChild(cardListEl);
+
+		const isCardListElExist = !!document.querySelector('.card__list');
+		assert.ok(isCardListElExist);
+	});
+
+	test("should be able to create header element via 'Header' class", (assert) => {
+		const header = new Header();
+		const headerEl = header.render();
+		document.body.appendChild(headerEl);
+
+		const isHeaderElExist = !!document.querySelector('header');
+		assert.ok(isHeaderElExist);
+	});
+
+	test("should be able to create section element via 'Section' class", (assert) => {
+		const section = new Section();
+		const sectionEl = section.render();
+		document.body.appendChild(sectionEl);
+
+		const isSectionElExist = !!document.querySelector('.section');
+		assert.ok(isSectionElExist);
+	});
+
+	test("should be able to create main element via 'Main' class", (assert) => {
+		const main = new Main();
+		const mainEl = main.render();
+		document.body.appendChild(mainEl);
+
+		const isMainElExist = !!document.querySelector('main');
+		assert.ok(isMainElExist);
+	});
+
+	test("should be able to create footer element via 'Footer' class", (assert) => {
+		const footer = new Footer();
+		const footerEl = footer.render();
+		document.body.appendChild(footerEl);
+
+		const isFooterElExist = !!document.querySelector('footer');
+		assert.ok(isFooterElExist);
+	});
+});
